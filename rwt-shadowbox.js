b44411de601b1b3b1c3e0d2bc786825a94cbb4e8
Joe Honton <joe@joe-honton.com>
2020-06-10 14:06:06 -0700

    deploy

--- rwt-shadowbox.js
+++ rwt-shadowbox.js
@@ -11,8 +11,11 @@
 
 export default class RwtShadowbox extends HTMLElement {
 
-	// The elementInstance is used to distinguish between multiple instances of this custom element
-	static elementInstance = 0;
+	static elementInstance = 1;
+	static htmlURL  = '/node_modules/rwt-shadowbox/rwt-shadowbox.blue';
+	static cssURL   = '/node_modules/rwt-shadowbox/rwt-shadowbox.css';
+	static htmlText = null;
+	static cssText  = null;
 
 	constructor() {
 		super();
@@ -24,50 +27,107 @@ export default class RwtShadowbox extends HTMLElement {
 		this.outerMargin = null;
 		
 		// properties
+		this.instance = RwtShadowbox.elementInstance++;
 		this.shortcutKey = null;
-		RwtShadowbox.elementInstance++;
 		this.collapseSender = `RwtShadowbox ${RwtShadowbox.elementInstance}`;
 
 		Object.seal(this);
 	}
 
 	//-------------------------------------------------------------------------
-	// customElement life cycle callbacks
+	// customElement life cycle callback
 	//-------------------------------------------------------------------------
 	async connectedCallback() {		
-		// guard against possible call after this has been disconnected
 		if (!this.isConnected)
 			return;
 
-		var htmlFragment = await this.fetchTemplate();
-		if (htmlFragment == null)
-			return;
-		
-		var styleElement = await this.fetchCSS();
-		if (styleElement == null)
-			return;
+		try {
+			var htmlFragment = await this.getHtmlFragment();
+			var styleElement = await this.getCssStyleElement();
 
-		var documentFragment = await this.fetchInnerHTML();
-		if (documentFragment != null) {
-			var elOuterMargin = htmlFragment.getElementById('outer-margin');
-			elOuterMargin.appendChild(documentFragment);
+			var documentFragment = await this.fetchInnerHTML();
+			if (documentFragment != null) {
+				var elOuterMargin = htmlFragment.getElementById('outer-margin');
+				elOuterMargin.appendChild(documentFragment);
+			}
+			
+			this.attachShadow({mode: 'open'});
+			this.shadowRoot.appendChild(htmlFragment); 
+			this.shadowRoot.appendChild(styleElement); 
+			
+			this.identifyChildren();
+			this.registerEventListeners();
+			this.initializeCaption();
+			this.initializeShortcutKey();
 		}
-		
-		 // append the HTML and CSS to the custom element's shadow root
-		this.attachShadow({mode: 'open'});
-		this.shadowRoot.appendChild(htmlFragment); 
-		this.shadowRoot.appendChild(styleElement); 
-		
-		this.identifyChildren();
-		this.registerEventListeners();
-		this.initializeCaption();
-		this.initializeShortcutKey();
+		catch (err) {
+			console.log(err.message);
+		}
 	}
 
 	//-------------------------------------------------------------------------
 	// initialization
 	//-------------------------------------------------------------------------
 
+	// Only the first instance of this component fetches the HTML text from the server.
+	// All other instances wait for it to issue an 'html-template-ready' event.
+	// If this function is called when the first instance is still pending,
+	// it must wait upon receipt of the 'html-template-ready' event.
+	// If this function is called after the first instance has already fetched the HTML text,
+	// it will immediately issue its own 'html-template-ready' event.
+	// When the event is received, create an HTMLTemplateElement from the fetched HTML text,
+	// and resolve the promise with a DocumentFragment.
+	getHtmlFragment() {
+		return new Promise(async (resolve, reject) => {
+			
+			document.addEventListener('html-template-ready', () => {
+				var template = document.createElement('template');
+				template.innerHTML = RwtShadowbox.htmlText;
+				resolve(template.content);
+			});
+			
+			if (this.instance == 1) {
+				var response = await fetch(RwtShadowbox.htmlURL, {cache: "no-cache", referrerPolicy: 'no-referrer'});
+				if (response.status != 200 && response.status != 304) {
+					reject(new Error(`Request for ${RwtShadowbox.htmlURL} returned with ${response.status}`));
+					return;
+				}
+				RwtShadowbox.htmlText = await response.text();
+				document.dispatchEvent(new Event('html-template-ready'));
+			}
+			else if (RwtShadowbox.htmlText != null) {
+				document.dispatchEvent(new Event('html-template-ready'));
+			}
+		});
+	}
+	
+	// Use the same pattern to fetch the CSS text from the server
+	// When the 'css-text-ready' event is received, create an HTMLStyleElement from the fetched CSS text,
+	// and resolve the promise with that element.
+	getCssStyleElement() {
+		return new Promise(async (resolve, reject) => {
+
+			document.addEventListener('css-text-ready', () => {
+				var styleElement = document.createElement('style');
+				styleElement.innerHTML = RwtShadowbox.cssText;
+				resolve(styleElement);
+			});
+			
+			if (this.instance == 1) {
+				var response = await fetch(RwtShadowbox.cssURL, {cache: "no-cache", referrerPolicy: 'no-referrer'});
+				if (response.status != 200 && response.status != 304) {
+					reject(new Error(`Request for ${RwtShadowbox.cssURL} returned with ${response.status}`));
+					return;
+				}
+				RwtShadowbox.cssText = await response.text();
+				document.dispatchEvent(new Event('css-text-ready'));
+			}
+			else if (RwtShadowbox.cssText != null) {
+				document.dispatchEvent(new Event('css-text-ready'));
+			}
+		});
+	}
+	
 	//^ Fetch the user-provided text from the file specified in
 	//  the custom element's sourceref attribute, which is a URL.
 	//  That file should contain valid HTML.
@@ -91,35 +151,6 @@ export default class RwtShadowbox extends HTMLElement {
 		template.innerHTML = templateText;
 		return template.content;
 	}
-
-	//^ Fetch the HTML template
-	//< returns a document-fragment suitable for appending to shadowRoot
-	//< returns null if server does not respond with 200 or 304
-	async fetchTemplate() {
-		var response = await fetch('/node_modules/rwt-shadowbox/rwt-shadowbox.blue', {cache: "no-cache", referrerPolicy: 'no-referrer'});
-		if (response.status != 200 && response.status != 304)
-			return null;
-		var templateText = await response.text();
-		
-		// create a template and turn its content into a document fragment
-		var template = document.createElement('template');
-		template.innerHTML = templateText;
-		return template.content;
-	}
-	
-	//^ Fetch the CSS styles and turn it into a style element
-	//< returns an style element suitable for appending to shadowRoot
-	//< returns null if server does not respond with 200 or 304
-	async fetchCSS() {
-		var response = await fetch('/node_modules/rwt-shadowbox/rwt-shadowbox.css', {cache: "no-cache", referrerPolicy: 'no-referrer'});
-		if (response.status != 200 && response.status != 304)
-			return null;
-		var css = await response.text();
-
-		var styleElement = document.createElement('style');
-		styleElement.innerHTML = css;
-		return styleElement;
-	}	
 	
 	//^ Identify this component's children
 	identifyChildren() {
@@ -231,6 +262,7 @@ export default class RwtShadowbox extends HTMLElement {
 			this.showDialog();
 		else
 			this.hideDialog();
+		event.stopPropagation();
 	}
 	
 	// retrieve and show
